656b6a1836a7bdc7871d043d9f3c4da7a3c3d00e
trabajo 1 <mh23-0285@unphu.edu.do>
2024-02-14 21:21:09 -0400

    first

---
+++ A2.java
@@ -0,0 +1,20 @@
+public class A2 {
+    public static void main(String[] args) {
+        String tIPO = "Computadora";
+
+        final double TASA_DOLAR= 59.50;
+
+        boolean dispnible = false;
+
+        final String MENSAJE_BIENVENIDA="Bienvenido a este modulo" ;
+
+         int resultado= 4*9;
+
+        double monto= 775.49;
+
+        double MontoConITBIS= 1.18 * monto;
+
+        System.out.println(MontoConITBIS);
+
+    }
+}
